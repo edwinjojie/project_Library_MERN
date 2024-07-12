42a5c11b17cf6e442d8ceae99244ac06057f30af
Varsha Sudeesh <varshasudeesh004@gmail.com>
2024-07-12 10:46:46 +0400

    correct commit

--- productApp/productApp/src/App.jsx
+++ productApp/productApp/src/App.jsx
@@ -2,19 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-<<<<<<< HEAD
-import { Signup } from './components/Signup'
-=======
->>>>>>> 494f3ab (correct commit)
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-<<<<<<< HEAD
-      <Signup/>
-=======
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -35,7 +28,6 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
->>>>>>> 494f3ab (correct commit)
     </>
   )
 }
